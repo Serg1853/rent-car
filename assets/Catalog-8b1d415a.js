65ba56f71dc89678b0ab150c590dbd63669ddf63
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2024-03-06 19:02:52 +0000

    Deploying to gh-pages from @ Serg1853/rent-car@aba06e1d68ee7576aad8f58fa48c574ef5113c33 🚀

---
+++ assets/Catalog-8b1d415a.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1932eab8.js";function o(){return t.jsx("h1",{children:"Catalog page"})}export{o as default};
---
+++ assets/Favorite-572bdb70.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-1932eab8.js";function e(){return r.jsx("h1",{children:"Favorite page"})}export{e as default};
