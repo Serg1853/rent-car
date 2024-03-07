452700570e184b49bf7ad2fc056605fae3cbe625
Serg1853 <sergey24226@gmail.com>
2024-03-07 23:12:14 +0200

    axios

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from 'react';
+import { useGetAdvertsQuery } from '../../redux/operations';
+
 function Catalog() {
+  const [cars, setCars] = useState([]);
+  const data = useGetAdvertsQuery();
+  console.log('data', data);
+  useEffect(() => {
+    if (data) {
+      setCars((prevCars) => [...prevCars, ...data]);
+    }
+  }, [data]);
+
   return <h1>Catalog page</h1>;
 }
 export default Catalog;
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,18 +1,34 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-// Define a service using a base URL and expected endpoints
-export const carsApi = createApi({
-  reducerPath: 'carsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://65e832be4bb72f0a9c4e8ae8.mockapi.io/',
-  }),
-  endpoints: (builder) => ({
-    getAdverts: builder.query({
-      query: () => `advert`,
-    }),
-  }),
-});
+axios.defaults.baseURL = 'https://65e832be4bb72f0a9c4e8ae8.mockapi.io/';
+export const fetchCars = createAsyncThunk(
+  'advert/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('avert');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-export const { useGetAdvertsQuery } = carsApi;
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// // Define a service using a base URL and expected endpoints
+// export const carsApi = createApi({
+//   reducerPath: 'carsApi',
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: 'https://65e832be4bb72f0a9c4e8ae8.mockapi.io/',
+//   }),
+//   endpoints: (builder) => ({
+//     getAdverts: builder.query({
+//       query: () => `advert`,
+//     }),
+//   }),
+// });
+
+// // Export hooks for usage in functional components, which are
+// // auto-generated based on the defined endpoints
+// export const { useGetAdvertsQuery } = carsApi;
