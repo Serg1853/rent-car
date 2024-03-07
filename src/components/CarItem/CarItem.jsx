95887e0f4383e23e38cfe1f85d2eb15151773a8f
Serg1853 <sergey24226@gmail.com>
2024-03-07 18:35:45 +0200

    Caritem

--- src/components/CarItem/CarItem.jsx
+++ src/components/CarItem/CarItem.jsx
@@ -4,6 +4,21 @@ function CarItem({ data }) {
       <div>
         <img src={data.img} alt="Car" />{' '}
       </div>
+      <div>
+        <h3>{data.make}</h3>
+        <span>{data.model}</span>,{data.year}
+        <p>{data.rentalPrice}</p>
+      </div>
+      <ui>
+        <li>{data.address.split(',')[1]}</li>
+        <li>{data.address.split(',')[2]}</li>
+        <li>{data.rentalCompany}</li>
+        <li>{data.type}</li>
+        <li>{data.model}</li>
+        <li>{data.mileage}</li>
+        <li>{data.accessories[0]}</li>
+      </ui>
+      <button>Learn more</button>
     </li>
   );
 }
