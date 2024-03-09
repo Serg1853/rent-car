a74859e816b7fd5d715f21e6d3e5124bb22d804c
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2024-03-09 14:29:36 +0000

    Deploying to gh-pages from @ Serg1853/rent-car@0d0bca14b261022956dbc329b506f7fb01422ceb 🚀

--- assets/Favorite-7f29a633.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-0a5b1b14.js";function e(){return r.jsx("h1",{children:"Favorite page"})}export{e as default};
---
+++ assets/Favorite-c23aab9d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-1857afab.js";function e(){return r.jsx("h1",{children:"Favorite page"})}export{e as default};
